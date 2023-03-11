7e4ee559dae6d383f5c5a5668ef233393c364d07
Tsonga Dog <Alimbetov_S0930@kt.nis.edu.kz>
2023-03-11 22:41:31 +0600

    CTA section

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -4,7 +4,7 @@ import styles from "../style.js";
 export const CTA = () => (
   <section className="py-8 sm:py-12">
     <div className="bg-black-gradient-2 flex flex-col place-content-center items-center justify-between gap-[60px] rounded-2xl px-[37px] py-[26px] ss:px-[57px] ss:py-[46px] md:flex-row md:px-[97px] md:py-[76px]">
-      <div className="flex w-3/4 flex-col gap-[35px] text-center md:text-start">
+      <div className="flex w-full flex-col gap-[35px] text-center md:w-3/4 md:text-start">
         <h2 className={`${styles.heading2}`}>Let’s try our service now!</h2>
         <p className={`${styles.paragraph}`}>
           Everything you need to accept card payments and grow your business
